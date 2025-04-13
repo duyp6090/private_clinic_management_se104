c81c1687fc1407627d7f849c53fa6b6d43f35190
iseT1enLoc <locnvt.it@gmail.com>
2025-04-13 12:35:59 +0700

    done drug_unit set up

--- src/main/java/com/example/demo/service/IDrugService.java
+++ src/main/java/com/example/demo/service/IDrugService.java
@@ -5,10 +5,29 @@
 
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.domain.Drugs;
+
 /**
  *
  * @author iset1enloc
  */
 public interface IDrugService {
 
+    // Create
+    Drugs createDrug(Drugs drug);
+
+    // Read all
+    List<Drugs> getAllDrugs();
+
+    // Read by ID
+    Optional<Drugs> getDrugById(Integer id);
+
+    // Update
+    Drugs updateDrug(Integer id, Drugs updatedDrug);
+
+    // Delete
+    void deleteDrug(Integer id);
 }
--- src/main/java/com/example/demo/service/IDrugUnitService.java
+++ src/main/java/com/example/demo/service/IDrugUnitService.java
@@ -5,10 +5,29 @@
 
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.domain.DrugUnits;
+
 /**
  *
  * @author iset1enloc
  */
 public interface IDrugUnitService {
 
+    // Create
+    DrugUnits createDrugUnit(DrugUnits drugUnit);
+
+    // Read all
+    List<DrugUnits> getAllDrugUnits();
+
+    // Read by ID (composite key)
+    Optional<DrugUnits> getDrugUnitById(int id);
+
+    // Update
+    DrugUnits updateDrugUnit(int id, DrugUnits updatedDrugUnit);
+
+    // Delete
+    void deleteDrugUnit(int id);
 }
