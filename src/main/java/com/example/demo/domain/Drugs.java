bfb0eedd042b871200659e39a5e4457f3b7973c7
Duymaoit <duyp6090@gmail.com>
2025-05-01 15:48:27 +0700

    fix dev 02

--- src/main/java/com/example/demo/domain/Drugs.java
+++ src/main/java/com/example/demo/domain/Drugs.java
@@ -1,10 +1,11 @@
 package com.example.demo.domain;
+
 import java.time.LocalDate;
-package com.example.demo.domain;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -80,4 +81,5 @@ public class Drugs {
     public void setDrugsUnit(DrugsUnit drugsUnit) {
         this.drugsUnit = drugsUnit;
     }
+
 }
